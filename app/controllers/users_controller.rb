142c1a1e9200744581dca3e4b84c31b1aaee882a
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-03-20 01:41:17 +0500

    Add splash screen section

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,16 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @users = User.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end
